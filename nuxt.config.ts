6745c1727080019cd86613602eb3e3a20ff374af
Dhua_N <13154593+dhua_n@user.noreply.gitee.com>
2025-08-28 18:53:54 +0800

    实现PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode'
+    '@nuxtjs/color-mode',
+    '@vite-pwa/nuxt',
   ],
   colorMode: {
     preference: 'system', // 默认跟随系统
@@ -11,4 +12,27 @@ export default defineNuxtConfig({
     classSuffix: ''       // 让 <html class="dark"> 而不是 class="dark-mode"
   }, 
   ssr: false,
+  pwa: {
+    registerType: 'autoUpdate',
+    devOptions: { enabled: true },        // 本地也能看
+    workbox: {
+      globPatterns: process.env.NODE_ENV === 'production'
+      ? ['**/*.{js,css,html,ico,woff2}']
+      : []
+    },
+    manifest: {
+      name: 'My Notes PWA',
+      short_name: 'Notes',
+      description: '离线 Markdown 笔记',
+      theme_color: '#3b82f6',
+      background_color: '#ffffff',
+      display: 'standalone'
+    }
+  },
+  app: {
+    head: {
+      link: [{ rel: 'manifest', href: '/manifest.webmanifest' }],
+      meta: [{ name: 'theme-color', content: '#3b82f6' }]
+    }
+  }
 })
